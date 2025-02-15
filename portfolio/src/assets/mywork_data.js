cb9c3f8a483d0278f70a7685dd86f86e2a156b84
ericpo1sh <ericpo1sh@gmail.com>
2025-02-15 12:50:55 -0600

    added new client website to my work section

--- portfolio/src/assets/mywork_data.js
+++ portfolio/src/assets/mywork_data.js
@@ -9,38 +9,38 @@ const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
+        w_img:project5_img,
+        w_link:"https://www.morgancustomwindows.com/"
+    },
+    {
+        w_no:2,
+        w_name:"Web design",
         w_img:project6_img,
         w_link:"https://nostalgiafy.store"
     },
     {
-        w_no:2,
+        w_no:3,
         w_name:"Web design",
         w_img:project1_img,
         w_link:"https://svitlanapavl.github.io/Hacksprint-2023/"
     },
     {
-        w_no:3,
+        w_no:4,
         w_name:"Web design",
         w_img:project2_img,
         w_link:"https://puzzleemptym.github.io/plant-clicker/"
     },
     {
-        w_no:4,
+        w_no:5,
         w_name:"Web design",
         w_img:project3_img,
         w_link:"https://github.com/O-01/holbertonschool-simple_shell/blob/master/README.md"
     },
     {
-        w_no:5,
+        w_no:6,
         w_name:"Web design",
         w_img:project4_img,
         w_link:"https://ericpo1sh.github.io/atlas-smiling-school-javascript/homepage.html"
-    },
-    {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project5_img,
-        w_link:"https://ericpo1sh.github.io/NovaSTVLLC/"
     },
 ]
  
